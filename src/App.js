c0c6e8a76c1297f8522f7eaf14fc7a8030a97c16
shreyaroyrini <63198532+shreyaroyrini@users.noreply.github.com>
2021-02-02 11:40:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+     <div className = "grid-container">
+       <header>
+         <a href = "/"> React Shopping Cart </a>
+       </header>
+       <main>
+         Product List
+       </main>
+       <footer>
+         All right is reserved.
+       </footer>
+     </div>
+  );
+}
+
+export default App;
